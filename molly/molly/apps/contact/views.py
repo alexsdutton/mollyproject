62919ecdaecdd8d77d21132dea9870dc65fc19e9
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-09-23 13:17:44 +0100

    Added a new renderer to the contact app's IndexView to support the upcoming AJAXification.

--- molly/molly/apps/contact/views.py
+++ molly/molly/apps/contact/views.py
@@ -1,8 +1,17 @@
-import simplejson, hashlib, urllib2
+import simplejson, hashlib, urllib
+
+# parse_qsl was moved from cgi to urlparse in Py2.6.
+try:
+    from urlparse import parse_qsl
+except ImportError, e:
+    from cgi import parse_qsl
+
 from django.http import HttpResponse
 from django.core.paginator import Paginator
+from django.template.loader import render_to_string
+from django.template import RequestContext
 
-from molly.utils.views import BaseView
+from molly.utils.views import BaseView, renderer
 from molly.utils.breadcrumbs import *
 
 from .forms import GenericContactForm
@@ -65,12 +74,17 @@ class ResultListView(IndexView):
                     request, context,
                     'There are no results for this page.',
                 )
-            page = paginator.page(page) 
+            page = paginator.page(page)
+
+            query_string = dict(parse_qsl(request.META.get('QUERY_STRING', '')))
+            query_string.pop('page', None)
+            query_string.pop('format', None)
 
             context.update({
                 'page': page,
                 'results': paginator,
                 'medium': medium,
+                'query_string': urllib.urlencode(query_string),
             })
 
         context['form'] = form
@@ -83,6 +97,20 @@ class ResultListView(IndexView):
 
         return self.render(request, context, 'contact/result_list')
 
+    @renderer(format='update')
+    def render_update(self, request, context, template_name):
+        new_context = {
+            'page_number': context['page'].number,
+            'page_count': context['results'].num_pages,
+            'people': [],
+        }
+        for result in context['page'].object_list:
+            context['person'] = result
+            new_context['people'].append(render_to_string(
+                'contact/result.html', context,
+                context_instance=RequestContext(request)))
+
+        return self.render_json(request, new_context, template_name)
 
 class ResultDetailView(BaseView):
     @BreadcrumbFactory
