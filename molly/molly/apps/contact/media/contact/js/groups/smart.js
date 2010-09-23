f3d016a94b0b9b3f14702de8ef4f437d01061fcf
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-09-23 15:55:49 +0100

    Tidying up CSS (and trying to work out why it doesn't work on Android).

--- molly/molly/apps/contact/media/contact/js/groups/smart.js
+++ molly/molly/apps/contact/media/contact/js/groups/smart.js
@@ -7,20 +7,29 @@ function contactMoreLink() {
 }
 
 function contactMore(e) {
+alert(e);
+try { 
     pageNumber += 1;
     loc = window.location;
     url = loc.protocol + '//' + loc.host + loc.pathname + '?format=update&' + queryString + '&page=' + pageNumber;
-    $('.contact-pagination-controls-bottom').html("Fetching&hellip;");
+    $('.contact-pagination-controls-bottom').html('<img src="'+media_url+'base/gif/loading.gif"/>');
     $.getJSON(url, function(data) {
       for (i in data.people) {
         person = $(data.people[i]).slideUp(0);
+        if (i == 0)
+          person.addClass('page-start');
         $('#contact-people').append(person);
       };
-      if (data.page_number == data.page_count)
-        $('.contact-pagination-controls-bottom').remove()
-      else
+      if (data.page_number == data.page_count) {
+        $('.contact-pagination-controls-bottom').remove();
+        $('#contact-people').addClass('last');
+      } else
         contactMoreLink()
         
     });
+    } catch (e) {
+    alert(e);
+    }
     return false;
+
 }
