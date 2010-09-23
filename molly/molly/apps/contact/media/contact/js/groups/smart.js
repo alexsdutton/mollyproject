beb27f57fcb4e032eb31288a273f34e773245344
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2010-09-23 13:30:59 +0100

    Initial JS for AJAX-based pagination. Doesn't work quite as it should on Android's WebKit browser.

--- molly/molly/apps/contact/media/contact/js/groups/smart.js
+++ molly/molly/apps/contact/media/contact/js/groups/smart.js
@@ -1,3 +1,26 @@
-$(function() {
-});
+$(contactMoreLink);
 
+function contactMoreLink() {
+  $('.contact-pagination-controls-bottom').html(
+    $('<a href="#" class="contact-pagination-more">More</a>').bind('click', contactMore)
+  );
+}
+
+function contactMore(e) {
+    pageNumber += 1;
+    loc = window.location;
+    url = loc.protocol + '//' + loc.host + loc.pathname + '?format=update&' + queryString + '&page=' + pageNumber;
+    $('.contact-pagination-controls-bottom').html("Fetching&hellip;");
+    $.getJSON(url, function(data) {
+      for (i in data.people) {
+        person = $(data.people[i]).slideUp(0);
+        $('#contact-people').append(person);
+      };
+      if (data.page_number == data.page_count)
+        $('.contact-pagination-controls-bottom').remove()
+      else
+        contactMoreLink()
+        
+    });
+    return false;
+}
